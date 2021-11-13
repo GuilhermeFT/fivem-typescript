e80e618ecc9c97383dd9fd463efa4d49a41d74b6
guilhermeft <guilhermetrind12@gmail.com>
2021-11-13 02:16:56 -0300

    feat: update files

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games {'gta5'}
+
+client_scripts {'dist/client.js'}
+server_scripts {'dist/server.js'}
+
+dependencies {'yarn', 'webpack'}
+
+webpack_config 'webpack.config.js'
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,18 @@
 const path = require('path');
-const fs = require('fs');
-const util = require('util');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-const readdir = util.promisify(fs.readdir);
-
-const configs = async () => {
-  const isDevelopment = 'production' !== 'production';
-  const clientFolderPath = path.join(__dirname, 'src', 'client');
-  const serverFolderPath = path.join(__dirname, 'src', 'server');
-
-  const clientFiles = await readdir(clientFolderPath);
-  const serverFiles = await readdir(serverFolderPath);
-
-  const clientPaths = clientFiles.map((file) =>
-    path.resolve(__dirname, 'src', 'client', file)
-  );
-
-  const serverPaths = serverFiles.map((file) =>
-    path.resolve(__dirname, 'src', 'server', file)
-  );
-
-  return {
-    mode: isDevelopment ? 'development' : 'production',
-    entry: {
-      client: clientPaths,
-      server: serverPaths,
-    },
-    output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, '../', 'dist'),
-    },
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(j|t)sx$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              plugins: [
-                isDevelopment && require.resolve('react-refresh/babel'),
-              ].filter(Boolean),
-            },
-          },
-        },
-        {
-          test: /\.scss$/,
-          exclude: /node_modules/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-      ],
-    },
-  };
+module.exports = {
+  mode: 'production',
+  entry: {
+    client: path.resolve(__dirname, 'src', 'client', 'index.ts'),
+    server: path.resolve(__dirname, 'src', 'server', 'index.ts'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    plugins: [new TsconfigPathsPlugin({})],
+  },
 };
-
-module.exports = configs;
